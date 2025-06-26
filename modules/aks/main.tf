d307c56c8bb22756be2113b25e7db037ace7dcc9
Richard Okoro <glasshouse747@gmail.com>
2025-06-26 09:46:40 +0100

    roboshop commit

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -47,3 +47,12 @@ resource "azurerm_kubernetes_cluster_node_pool" "main" {
   vnet_subnet_id              = var.vnet_subnet_id
 }
 
+output "aks" {
+  value = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
+}
+
+resource "azurerm_role_assignment" "aks-to-acr" {
+  scope                = data.azurerm_container_registry.main.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.main.kubelet_identity[0].object_id
+}
