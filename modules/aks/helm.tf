b4ffc99b007268b052dbf499da76d2c3f5390be5
Richard Okoro <glasshouse747@gmail.com>
2025-09-06 20:19:51 +0100

    roboshop commit

---
+++ modules/aks/helm.tf
@@ -0,0 +1,53 @@
+resource "helm_release" "external-secrets" {
+  depends_on = [
+    null_resource.kubeconfig
+  ]
+
+  name             = "external-secrets"
+  repository       = "https://charts.external-secrets.io"
+  chart            = "external-secrets"
+  namespace        = "devops"
+  version          = "0.9.13"
+  create_namespace = true
+
+  values = [<<EOF
+installCRDs: true
+EOF
+  ]
+}
+
+resource "null_resource" "external-secrets-secret-store" {
+  depends_on = [
+    helm_release.external-secrets
+  ]
+
+  provisioner "local-exec" {
+    command = <<TF
+kubectl apply -f - <<KUBE
+apiVersion: external-secrets.io/v1
+kind: ClusterSecretStore
+metadata:
+  name: roboshop-${var.env}
+spec:
+  provider:
+    vault:
+      server: "http://vault-int.mydevops.shop:8200"
+      path: "roboshop-${var.env}"
+      version: "v2"
+      auth:
+        tokenSecretRef:
+          name: "vault-token"
+          key: "token"
+          namespace: devops
+---
+apiVersion: v1
+kind: Secret
+metadata:
+  name: vault-token
+  namespace: devops
+data:
+  token: ${base64encode(var.token)}
+KUBE
+TF
+  }
+}
