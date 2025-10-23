6864d0ebe06a605ab1b0ff3b8193ee5b192d0f6b
Richard Okoro <glasshouse747@gmail.com>
2025-10-23 05:19:13 +0100

    roboshop commit

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -5,5 +5,11 @@ resource "azurerm_virtual_network" "main" {
   address_space       = var.address_space
 }
 
-
+resource "azurerm_subnet" "main" {
+  for_each             = var.subnets
+  name                 = "${each.key}-subnet"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = each.value["address_prefixes"]
+}
 
