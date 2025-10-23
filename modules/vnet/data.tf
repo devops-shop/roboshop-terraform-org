0169f96e6b7b09e72aec1d5cf6efeddc36ec0a70
Richard Okoro <glasshouse747@gmail.com>
2025-10-23 06:52:11 +0100

    roboshop commit

---
+++ modules/vnet/data.tf
@@ -0,0 +1,4 @@
+data "azurerm_virtual_network" "tools" {
+  name                = "main"
+  resource_group_name = "project-setup-1"
+}
--- variables.tf
+++ variables.tf
@@ -10,5 +10,5 @@ variable "dns_record_rg_name" {}
 variable "token" {}
 variable "aks" {}
 variable "vnets" {}
-
+variable "tools_vnet_resource_id" {}
 
