c332a27fa335e010cb499f1f428a83664d4cc3b4
Richard Okoro <glasshouse747@gmail.com>
2025-10-23 12:20:16 +0100

    roboshop commit

--- modules/vnet/main.tf
+++ modules/vnet/main.tf
@@ -1,4 +1,4 @@
-resource "azurerm_virtual_network" "test-network" {
+resource "azurerm_virtual_network" "main" {
   name                = var.name
   location            = var.location
   resource_group_name = var.resource_group_name
@@ -9,14 +9,14 @@ resource "azurerm_subnet" "main" {
   for_each             = var.subnets
   name                 = "${each.key}-subnet"
   resource_group_name  = var.resource_group_name
-  virtual_network_name = azurerm_virtual_network.test-network.name
+  virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = each.value["address_prefixes"]
 }
 
 resource "azurerm_virtual_network_peering" "here-to-tools" {
   name                      = "${var.name}-conn-to-tools"
   resource_group_name       = var.resource_group_name
-  virtual_network_name      = azurerm_virtual_network.test-network.name
+  virtual_network_name      = azurerm_virtual_network.main.name
   remote_virtual_network_id = var.tools_vnet_resource_id
 }
 
@@ -25,7 +25,7 @@ resource "azurerm_virtual_network_peering" "tools-to-here" {
   name                      = "tools-conn-to-${var.name}"
   resource_group_name       = data.azurerm_virtual_network.tools.resource_group_name
   virtual_network_name      = data.azurerm_virtual_network.tools.name
-  remote_virtual_network_id = azurerm_virtual_network.test-network.id
+  remote_virtual_network_id = azurerm_virtual_network.main.id
 }
 
 
